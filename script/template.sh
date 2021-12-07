055e724feeeadf18c3e3379dcfde05f9e4c6b093
SuperCuteXiaoSi <1531733886@qq.com>
2021-12-07 17:02:56 +0800

    feature: 添加脚本

---
+++ script/template.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+NAME=$1
+
+# 打印组件目录的完整地址
+FILE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")/../packages" && pwd)
+
+# 启动脚本后面是否有写 name
+if [ "$#" -ne 1 ] || [[ $NAME =~ $re ]] || [ "$NAME" == "" ]; then
+  echo -e "\033[0;31m[ERROR] npm run ct \${name} 输入的字符错误 \033[0m"
+  exit 1
+fi
+
+# 拼接组件存放路径
+DIRNAME="$FILE_PATH/components/$NAME"
+
+# 判断此路径是否有想同的组件
+if [ -d "$DIRNAME" ]; then
+  echo -e "\033[0;31m[ERROR] $NAME 已经存在相同的组件 \033[0m"
+  exit 1
+fi
+
+# 生成组件目录
+mkdir -p "$DIRNAME"
+mkdir -p "$DIRNAME/src"
+
+# 生成文件.vue 并写入模板
+cat > $DIRNAME/src/index.vue <<EOF
+<template>
+  <div>
+  </div>
+</template>
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'I${NAME}',
+  props: { },
+  setup(props) {
+    // init here
+  },
+})
+</script>
+<style>
+</style>
+EOF
+
+# 生成导入模板文件 index.ts
+cat <<EOF >"$DIRNAME/index.ts"
+import type { App } from 'vue'
+import ${NAME} from './src/index.vue'
+// import type { SFCWithInstall } from "../types";
+
+type SFCWithInstall<T> = T | { install(app: App): void } // vue 安装
+
+${NAME}.install = (app: App) => {
+	app.component(${NAME}.name, ${NAME})
+}
+
+const _${NAME}: SFCWithInstall<typeof ${NAME}> = ${NAME} // 增加类型
+export default _${NAME}
+EOF
