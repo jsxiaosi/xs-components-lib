670b3cc2a6279ee8d1be95798cc3178138c9c470
SuperCuteXiaoSi <1531733886@qq.com>
2022-01-12 22:03:35 +0800

    feature: 打包组件库

--- lib/components/button/index.d.ts
+++ lib/components/button/index.d.ts
@@ -1,9 +1,4 @@
-import type { App } from 'vue'
 import IButton from './src/index.vue'
-declare type SFCWithInstall<T> =
-	| T
-	| {
-			install(app: App): void
-	  }
+import type { SFCWithInstall } from '../types'
 declare const InMeAccordionItem: SFCWithInstall<typeof IButton>
 export default InMeAccordionItem
